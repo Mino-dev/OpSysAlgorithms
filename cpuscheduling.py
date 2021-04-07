7dceb11b82d29413efe38ebdf88579cdf4b13b9d
Mino-dev <201720078@fit.edu.ph>
2021-04-07 21:46:40 +0800

    Finished all algorithms tackled in the class, still need to double check

--- cpuscheduling.py
+++ cpuscheduling.py
@@ -1,8 +1,106 @@
 import argparse
 from argparse import RawTextHelpFormatter
 from dataclasses import dataclass
+import sys
 
 queue = []
+def rr(n, timeslice):
+
+   
+    total_wait  =  total_turn = complete = time = 0
+    
+    while complete != n:
+
+        for i in range(n):
+            
+            if not queue[i].isCompleted:
+                if queue[i]._burst <= timeslice:
+
+                    time += queue[i]._burst
+                    queue[i]._burst = 0
+
+                else:
+
+                    queue[i]._burst -= timeslice
+                    time += timeslice
+
+                if queue[i]._burst == 0:
+
+                        queue[i].isCompleted = True
+                        complete += 1
+
+                        queue[i].completion = time
+
+                        queue[i].turnaround = queue[i].completion - queue[i].arrival
+
+                        queue[i].waiting = queue[i].turnaround - queue[i].burst
+
+                        total_turn += queue[i].turnaround
+                        total_wait += queue[i].waiting
+                
+    return total_wait, total_turn
+
+    
+def computePreemptive(n, priority = False):
+    global queue
+    
+    completed = time = total_turn = total_wait = 0
+    while completed != n:
+
+        idx = -1
+        if priority:
+            low = -1
+        else:
+            low = sys.maxsize
+            
+        for i in range(n):
+            
+            if not queue[i].isCompleted and time >= queue[i].arrival:
+                
+                if priority:
+
+                    if low < queue[i].priority:
+                        
+                        low = queue[i].priority
+                        idx = i
+                    
+                    if low == queue[i].priority:
+                        
+                        if queue[i].arrival < queue[idx].arrival:
+                            
+                            low = queue[i].priority
+                            idx = i
+                else:
+
+                    if low > queue[i]._burst:
+
+                        low = queue[i]._burst
+                        idx = i
+
+                    if low == queue[i]._burst:
+
+                        if queue[i].arrival < queue[idx].arrival:
+                            low = queue[i]._burst
+                            idx = i
+        time += 1
+
+        if idx != -1:
+
+            queue[idx]._burst -= 1
+            
+            if queue[idx]._burst == 0:
+
+                queue[idx].completion = time
+                queue[idx].turnaround = queue[idx].completion - queue[idx].arrival  
+                queue[idx].waiting = queue[idx].turnaround - queue[idx].burst
+
+                total_wait += queue[idx].waiting
+                total_turn += queue[idx].turnaround
+                
+                completed += 1
+                queue[idx].isCompleted = True
+
+    return total_wait, total_turn
 
 def computeNonPreemptive(n, prio = False):
 
@@ -25,7 +123,6 @@ def computeNonPreemptive(n, prio = False):
             temp[0].completion = time
             temp[0].turnaround = abs(temp[0].completion - temp[0].arrival)               
             temp[0].waiting = abs(temp[0].turnaround - temp[0].burst)
-            temp[0].isCompleted = True
             
             total_wait += temp[0].waiting
             total_turn += temp[0].turnaround
@@ -80,7 +177,7 @@ def main():
             process.priority = int(input("Enter the priority: "))
 
         queue.append(process)
-
+    
     #sort by arrival
     queue = sorted(queue, key=lambda l: l.arrival)
 
@@ -88,15 +185,25 @@ def main():
         wait, turn = computeNonPreemptive(numberOfProcess)
     elif args.func == "nprio":
         wait, turn = computeNonPreemptive(numberOfProcess, True)
+    elif args.func == "srtf":
+        wait, turn = computePreemptive(numberOfProcess)
+    elif args.func == "prio":
+        wait, turn = computePreemptive(numberOfProcess, True)
+    elif args.func == "rr":
+        timeslice = int(input("Enter the time quantum of the system: "))
+        wait, turn = rr(numberOfProcess, timeslice)
     else:
         wait, turn = fcfs(numberOfProcess)
     
-    print("Process ID\tArrival Time\tBurst Time\tWaiting Time\tTurnaround Time\n")
+    print("Process ID\tArrival Time\tBurst Time\tWaiting Time\tTurnaround Time")
     for i in range(numberOfProcess):
         print(f"{queue[i].P}\t\t{queue[i].arrival}\t\t{queue[i].burst}\t\t{queue[i].waiting}\t\t{queue[i].turnaround}")
 
     print("Average Waiting Time: {:.2f} ms".format(wait / numberOfProcess))
     print("Average Turnaround Time: {:.2f} ms".format(turn / numberOfProcess))
+
+    for i in queue:
+        print(i)
 @dataclass
 class Process:
     P: int
